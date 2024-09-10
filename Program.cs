4846b65e733a931bb8a32cb94f839a712f7c3204
Nazlı Sunay Bulbul <nazlisunaybulbul@gmail.com>
2024-09-10 14:11:18 +0300

    Detay ve kirala eklendi

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace CarRental
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(15);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,7 +32,7 @@ namespace CarRental
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseSession();
             app.UseAuthorization();
 
             app.MapControllerRoute(
