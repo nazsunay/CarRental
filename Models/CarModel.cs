208ebcc27541b0e4504362ef86e666d55ea49a83
Nazlı Sunay Bulbul <nazlisunaybulbul@gmail.com>
2024-09-11 18:27:18 +0300

    Rent Controller eklendi ve login düzenlendi

--- Models/CarModel.cs
+++ Models/CarModel.cs
@@ -12,14 +12,19 @@
         public decimal DailyRate { get; set; }
         public bool IsAvailable { get; set; }
         public string ImgUrl { get; set; }
-       
+        public DateTime RentalDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string PickupLocation { get; set; }
+
     }
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
+
+       public string Password { get; set; }
     }
     public class Rental
     {
@@ -29,6 +34,7 @@
         public DateTime RentalDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public decimal TotalCost { get; set; }
+         public string PickupLocation { get; set; }
         public Car Car { get; set; }
     }
 
